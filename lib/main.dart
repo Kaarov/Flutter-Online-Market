1b1e09af70c4d2cf57c9fd66758a78b29a9c7322
Anas Kaarov <anasbinkurban@gmail.com>
2023-12-03 21:46:47 +0600

    refactored main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:online_market/pages/home.dart';
+import 'package:online_market/pages/login.dart';
+import 'package:online_market/pages/main.dart';
+import 'package:online_market/pages/signup.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -9,7 +14,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Mobile App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routes: {
+        '/': (context) => MainPage(),
+        '/login': (context) => LoginPage(),
+        '/signup': (context) => SignUpPage(),
+        '/home': (context) => Home(),
+      },
+    );
   }
 }
 
